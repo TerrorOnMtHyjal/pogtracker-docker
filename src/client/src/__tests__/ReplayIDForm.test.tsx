8ee0ec55dba108322438f29bbf58f5f26fba867c
Jared Mohney <jared.mohney@gmail.com>
2019-05-01 21:15:31 -0600

    install prettier

--- src/client/src/__tests__/ReplayIDForm.test.tsx
+++ src/client/src/__tests__/ReplayIDForm.test.tsx
@@ -1,34 +1,38 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ReplayIDForm from '../components/ReplayIDForm';
+import React from "react";
+import { shallow } from "enzyme";
+import ReplayIDForm from "../components/ReplayIDForm";
 
 const defaultProps = {
-  input: '1337',
+  input: "1337",
   setInput: () => {},
   processForm: () => {}
 };
 
-describe('<ReplayIDForm />', () => {
-  it('renders', () => {
-    shallow(<ReplayIDForm {...defaultProps}/>)
+describe("<ReplayIDForm />", () => {
+  it("renders", () => {
+    shallow(<ReplayIDForm {...defaultProps} />);
   });
 
-  it('runs props.setInput on text input field change', () => {
+  it("runs props.setInput on text input field change", () => {
     const setInputMock = jest.fn();
-    const mockEvent = { target: { value: 'test input'}};
-    const wrapper = shallow(<ReplayIDForm {...defaultProps} setInput={setInputMock}/>);
+    const mockEvent = { target: { value: "test input" } };
+    const wrapper = shallow(
+      <ReplayIDForm {...defaultProps} setInput={setInputMock} />
+    );
 
-    wrapper.find("input[type='text']").simulate('change', mockEvent);
+    wrapper.find("input[type='text']").simulate("change", mockEvent);
 
     expect(setInputMock).toBeCalledTimes(1);
-    expect(setInputMock).toBeCalledWith('test input');
+    expect(setInputMock).toBeCalledWith("test input");
   });
 
-  it('runs props.processForm on form submission', () => {
+  it("runs props.processForm on form submission", () => {
     const processFormMock = jest.fn();
-    const wrapper = shallow(<ReplayIDForm {...defaultProps} processForm={processFormMock}/>);
+    const wrapper = shallow(
+      <ReplayIDForm {...defaultProps} processForm={processFormMock} />
+    );
 
-    wrapper.find('form').simulate('submit');
+    wrapper.find("form").simulate("submit");
     expect(processFormMock).toBeCalledTimes(1);
   });
 });
