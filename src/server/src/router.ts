352dd3023c5ab9fe53f6e725cbf810cc868f2a05
Jared Mohney <jared.mohney@gmail.com>
2019-06-11 03:36:20 -0600

    Add fetchComments util

--- src/server/src/router.ts
+++ src/server/src/router.ts
@@ -1,9 +1,11 @@
 import { Router, Application, Request, Response } from "express";
+import fetchComments from "./utils/fetchComments";
 
 const router = (app: Application) => {
   const apiRouter: Router = Router();
 
   app.get("/api/process", (req: Request, res: Response) => {
+    const fetched = fetchComments(req.query.replayID);
     res.status(200).json({ message: "FEZZZZZ" });
   });
 
